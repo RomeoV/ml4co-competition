711bbe1baa024a50b3e89768d7c269635f2b2d00
Romeo Valentin <rvalentin@student.ethz.ch>
2021-09-30 19:20:27 +0200

    Make evaluation callback
    The callback judges "how well we're doing" in the sense of the
    competition.
    It queries the NN for the best configuration choice and then computes
    the mean primal_dual_integral given the data with that configuration
    choice. Then it computes the percentile of that integral and returns the
    average of this metric over all instances.

--- bo_gnn/config/train_gnn.py
+++ bo_gnn/config/train_gnn.py
@@ -3,6 +3,9 @@ import re
 import os
 import random
 import pickle
+import pathlib
+import itertools
+import scipy
 from typing import Tuple
 
 import torch
@@ -11,10 +14,12 @@ import numpy as np
 import pytorch_lightning as pl
 import torch_geometric as tg
 
+import pytorch_lightning
 from pytorch_lightning import Trainer
 from models.baseline import ConfigPerformanceRegressor
 from data_utils.dataset import MilpDataset, Folder, DataFormat
-from torch_geometric.data import Data, DataLoader
+from data_utils.milp_data import MilpBipartiteData
+from torch_geometric.data import Data, DataLoader, Batch
 
 
 class MilpGNNTrainable(pl.LightningModule):
@@ -52,8 +57,107 @@ class MilpGNNTrainable(pl.LightningModule):
         return torch.optim.Adam(self.parameters(), lr=self.hparams.initial_lr)
 
 
+class EvaluatePredictedParametersCallback(pytorch_lightning.callbacks.Callback):
+    def on_train_epoch_end(self, trainer, pl_module):
+        def find_best_configs(model, instance, initial_primal, initial_dual, timelimit):
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            all_config_inputs = torch.stack(
+                [
+                    torch.tensor(
+                        [a, b, c, initial_primal, initial_dual, timelimit],
+                        dtype=torch.float32,
+                    )
+                    for a, b, c in itertools.product(range(4), range(4), range(4))
+                ],
+                axis=0,
+            ).to(device)
+
+            instance_batch = Batch.from_data_list(
+                [instance] * len(all_config_inputs)
+            ).to(device)
+
+            model.eval()
+            preds = model((instance_batch, all_config_inputs))
+            model.train()
+
+            best_config_id = {}
+            best_config_id["mean"] = preds[:, 0].argmin()
+            best_config_id["optimistic"] = (preds[:, 0] - preds[:, 1]).argmin()
+            best_config_id["pessimistic"] = (preds[:, 0] + preds[:, 1]).argmin()
+
+            best_config = {
+                k: all_config_inputs[v, 0:3].to(torch.int32)
+                for k, v in best_config_id.items()
+            }
+
+            return best_config
+
+        def percentile_of_config(config, instance, df):
+            pred_with_config = df[
+                (df.instance_file == instance)
+                & (df.presolve_config_encoding == int(config[0]))
+                & (df.heuristic_config_encoding == int(config[1]))
+                & (df.separating_config_encoding == int(config[2]))
+            ].time_limit_primal_dual_integral.median()
+
+            percentile = scipy.stats.percentileofscore(
+                df[df.instance_file == instance].time_limit_primal_dual_integral,
+                pred_with_config,
+            )
+
+            return percentile
+
+        def get_instance_data(instance_file):
+            # TODO clean this up
+            path = pathlib.Path(f"../../instances/1_item_placement/train")
+
+            with open(
+                os.path.join(path, instance_file.replace(".mps.gz", ".pkl")),
+                "rb",
+            ) as infile:
+                instance_description_pkl = pickle.load(infile)
+                instance_data = MilpBipartiteData(
+                    var_feats=instance_description_pkl.variable_features,
+                    cstr_feats=instance_description_pkl.constraint_features,
+                    edge_indices=instance_description_pkl.edge_features.indices,
+                    edge_values=instance_description_pkl.edge_features.values,
+                )
+            return instance_data
+
+        percentiles = {"mean": [], "optimistic": [], "pessimistic": []}
+        val_data_df = trainer.val_dataloaders[0].dataset.csv_data_full
+
+        # for instance in np.random.choice(val_data_df.instance_file.unique(), 30):
+        for instance in val_data_df.instance_file.unique():
+            general_config = val_data_df[val_data_df.instance_file == instance].iloc[
+                0, 3:6
+            ]  # timelimit, initial_primal, initial_dual
+            print("find best configs")
+            best_configs = find_best_configs(
+                pl_module.model,
+                get_instance_data(instance),
+                general_config[0],
+                general_config[1],
+                general_config[2],
+            )
+
+            print("compute percentiles")
+            for k, v in best_configs.items():
+                percentiles[k].append(percentile_of_config(v, instance, val_data_df))
+
+        percentile_means = {
+            f"{k}_pred_percentile": torch.tensor(v).mean()
+            for k, v in percentiles.items()
+        }
+        self.log_dict(percentile_means, prog_bar=True)
+
+
 def main():
-    trainer = Trainer(max_epochs=3, gpus=1 if torch.cuda.is_available() else 0)
+    trainer = Trainer(
+        max_epochs=100,
+        gpus=1 if torch.cuda.is_available() else 0,
+        callbacks=[EvaluatePredictedParametersCallback()],
+    )
     model = MilpGNNTrainable(config_dim=6)
     data_train = DataLoader(
         MilpDataset(
