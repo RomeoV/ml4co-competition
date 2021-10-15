ad372c3d73725de02ffb23639e5f96ce52ba0071
Claudio Ferrari <ferraric@ethz.ch>
2021-10-15 12:20:30 +0200

    another try

--- bo_gnn/config/init.sh
+++ bo_gnn/config/init.sh
@@ -7,6 +7,7 @@ conda env create -n ml4co -f conda.yaml
 conda activate ml4co
 
 # additional installation commands go here
+pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
 pip install torch-scatter -f https://data.pyg.org/whl/torch-1.9.0+cu111.html
 pip install torch-sparse -f https://data.pyg.org/whl/torch-1.9.0+cu111.html
 pip install torch-geometric
