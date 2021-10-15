f754b889ec835c8939e16997ef632cc09df49c49
Claudio Ferrari <ferraric@ethz.ch>
2021-10-15 14:35:45 +0200

    update package versions

--- bo_gnn/config/init.sh
+++ bo_gnn/config/init.sh
@@ -10,6 +10,6 @@ conda activate ml4co
 pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
 pip install torch-scatter -f https://data.pyg.org/whl/torch-1.9.0+cu111.html
 pip install torch-sparse -f https://data.pyg.org/whl/torch-1.9.0+cu111.html
-pip install torch-geometric
+pip install torch-geometric==1.7.2
 
 conda deactivate
