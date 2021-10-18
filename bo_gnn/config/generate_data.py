ea713d556d20e508715f01aa4e40b0b53db2e43f
Chris Wendler <wendlerc@eu-login-37.euler.ethz.ch>
2021-10-18 11:55:13 +0200

    change random seed to random

--- bo_gnn/config/generate_data.py
+++ bo_gnn/config/generate_data.py
@@ -9,6 +9,7 @@ import argparse
 import tempfile
 import subprocess
 import itertools
+import numpy as np
 
 from filelock import FileLock
 import pathlib
@@ -179,13 +180,13 @@ def solve_instances_and_periodically_write_to_file(
         for instance in instance_paths:
             number_of_configs = len(selected_config_ids)
             all_instances += [instance] * number_of_configs * number_of_random_seeds
-            all_actions += [{'config_id': config_id, 'random_seed': random_seed} for config_id, random_seed in
+            all_actions += [{'config_id': config_id, 'random_seed': np.random.randint(100000)} for config_id, random_seed in
                             itertools.product(selected_config_ids, range(number_of_random_seeds))]
     else:
         for instance in instance_paths:
             number_of_configs = total_number_of_configs
             all_instances += [instance] * number_of_configs * number_of_random_seeds
-            all_actions += [{'config_id': config_id, 'random_seed': random_seed} for config_id, random_seed in
+            all_actions += [{'config_id': config_id, 'random_seed': np.random.randint(100000)} for config_id, random_seed in
                             itertools.product(range(number_of_configs), range(number_of_random_seeds))]
 
     print("Running every instance with {} configs".format(number_of_configs))
