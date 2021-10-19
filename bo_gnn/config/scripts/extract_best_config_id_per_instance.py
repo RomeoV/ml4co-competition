d5a8e047138c118de852af445143b1cdefc6f963
Claudio Ferrari <ferraric@ethz.ch>
2021-10-19 18:21:30 +0200

    save instance to best config id as dict

--- bo_gnn/config/scripts/extract_best_config_id_per_instance.py
+++ bo_gnn/config/scripts/extract_best_config_id_per_instance.py
@@ -1,19 +1,19 @@
-import csv
+import pickle as pkl
+
 import pandas as pd
 
 if __name__ == "__main__":
     base_path = "../data/3_anonymous/"
     performance_data = pd.read_csv(base_path + "3_anonymous_results_118.csv")
-    save_path = base_path + "best_config_id_by_instance.csv"
+    save_path = base_path + "best_config_id_by_instance.pkl"
 
-    best_config_id_per_instance = []
+    best_config_id_per_instance = {}
     for instance in performance_data.instance_file.unique():
         performance_data_for_instance = performance_data[performance_data.instance_file == instance]
         row_index_of_best_performance = performance_data_for_instance.time_limit_primal_dual_integral.argmin()
         best_config_id = performance_data_for_instance.config_id.iloc[row_index_of_best_performance]
 
-        best_config_id_per_instance.append([instance, best_config_id])
+        best_config_id_per_instance[instance] = best_config_id
 
-    with open(save_path, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(best_config_id_per_instance)
+    with open(save_path, "wb") as f:
+        pkl.dump(best_config_id, f)
