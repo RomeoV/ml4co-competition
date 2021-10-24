dc5fb0c89c1dfabcd34ce823cb79f17f50c4343d
Claudio Ferrari <ferraric@ethz.ch>
2021-10-24 22:12:47 +0200

    use cuda 10.2

--- bo_gnn/config/init.sh
+++ bo_gnn/config/init.sh
@@ -7,9 +7,9 @@ conda env create -n ml4co -f conda.yaml
 conda activate ml4co
 
 # additional installation commands go here
-pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
-pip install torch-scatter -f https://data.pyg.org/whl/torch-1.9.0+cu111.html
-pip install torch-sparse -f https://data.pyg.org/whl/torch-1.9.0+cu111.html
-pip install torch-geometric==1.7.2 -f https://data.pyg.org/whl/torch-1.9.0+cu111.html
+pip install torch==1.9.1+cu102 torchvision==0.10.1+cu102 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch-scatter -f https://data.pyg.org/whl/torch-1.9.0+cu102.html
+pip install torch-sparse -f https://data.pyg.org/whl/torch-1.9.0+cu102.html
+pip install torch-geometric==1.7.2 -f https://data.pyg.org/whl/torch-1.9.0+cu102.html
 
 conda deactivate
