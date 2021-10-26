ad7ecc037c28b5107f2d4fa802a4603f7ddfc01c
Claudio Ferrari <ferraric@ethz.ch>
2021-10-26 19:57:14 +0200

    added necessary mappings for agent

--- bo_gnn/make_submission.sh
+++ bo_gnn/make_submission.sh
@@ -6,6 +6,8 @@ cp -r config/param_configurations MixedInspiringLamePuns
 cp -r config/trained_model_checkpoints MixedInspiringLamePuns
 cp -r config/models MixedInspiringLamePuns
 cp config/train_gnn.py MixedInspiringLamePuns
+cp config/models/callbacks.py MixedInspiringLamePuns
+cp -r config/parameter_configuration_mapping MixedInspiringLamePuns
 cp -r config/data_utils MixedInspiringLamePuns
 zip -r config/MixedInspiringLamePuns.zip MixedInspiringLamePuns
 mv MixedInspiringLamePuns ../submissions
