e4e319d73b875e69567a74b221e279b65dd7df96
Claudio Ferrari <ferraric@ethz.ch>
2021-10-26 19:57:59 +0200

    adapt to new model structure - add nn model for task 3

--- bo_gnn/config/agents/config.py
+++ bo_gnn/config/agents/config.py
@@ -1,14 +1,16 @@
 import os
-from typing import Any, Dict
+import pickle as pkl
+from typing import Any, Dict, Tuple
 import sys
 
-sys.path.insert(0,'..')
+sys.path.insert(0, '..')
 
+import numpy as np
 import pytorch_lightning as pl
 import torch
 from train_gnn import MilpGNNTrainable
+from callbacks import EvaluatePredictedParametersCallback
 from ecole.observation import MilpBipartite
-from torch import Tensor
 from torch_geometric.data import Batch
 
 from data_utils.milp_data import MilpBipartiteData
@@ -16,13 +18,13 @@ from data_utils.milp_data import MilpBipartiteData
 CHECKPOINT_BASE_PATH = "trained_model_checkpoints/"
 PARAMETER_CONFIGURATIONS_PATH = "param_configurations/"
 
+
 class ObservationFunction(MilpBipartite):
 
     def __init__(self, problem):
         # called once for each problem benchmark
         super(ObservationFunction, self).__init__()
         self.problem = problem  # to devise problem-specific observations
-
 
     def seed(self, seed):
         # called before each episode
@@ -34,18 +36,53 @@ class ObservationFunction(MilpBipartite):
         pass
 
     def extract(self, model, done):
-        return super(ObservationFunction, self).extract(model, done)
+        if self.problem == "anonymous":
+            m = model.as_pyscipopt()
+            nvars = m.getNVars()
+            nconss = m.getNConss()
+            return np.array((nvars, nconss))
+        else:
+            return super(ObservationFunction, self).extract(model, done)
+
+
+class DummyNNModel:
+    def __init__(self):
+        pass
+
+    def predict(self, x: np.ndarray) -> int:
+        return 209
+
 
 class Policy():
 
     def __init__(self, problem):
         # called once for each problem benchmark
         self.problem = problem  # to devise problem-specific policies
-        files_in_checkpoint_dir = os.listdir(CHECKPOINT_BASE_PATH + problem)
-        assert len(files_in_checkpoint_dir) == 1
-        model_path = CHECKPOINT_BASE_PATH + problem + "/" + files_in_checkpoint_dir[0]
-        self.performance_prediction_model = MilpGNNTrainable.load_from_checkpoint(model_path)
-        self.performance_prediction_model.eval()
+
+        if self.problem == "anonymous":
+            self.best_config_id_prediction_model = DummyNNModel()
+        else:
+            files_in_checkpoint_dir = os.listdir(CHECKPOINT_BASE_PATH + problem)
+            assert len(files_in_checkpoint_dir) == 1
+            model_path = CHECKPOINT_BASE_PATH + problem + "/" + files_in_checkpoint_dir[0]
+
+            with open('parameter_configuration_mapping/config_tuples_to_ids.pkl', 'rb') as handle:
+                self.config_tuple_to_id_mapping = pkl.load(handle)
+
+            if self.problem == "item_placement":
+                self.config_index_to_tuple_mapping = np.load('parameter_configuration_mapping/unique_configs_in_dataset_p1.npy', allow_pickle=True)
+                self.performance_prediction_model = MilpGNNTrainable(config_dim=60, optimizer=None, initial_lr=None, batch_size=None, git_hash=None,
+                                                                     problem=None, n_gnn_layers=4,
+                                                                     gnn_hidden_dim=64, ensemble_size=3).load_from_checkpoint(model_path)
+            elif self.problem == "load_balancing":
+                self.config_index_to_tuple_mapping = np.load('parameter_configuration_mapping/unique_configs_in_dataset_p2.npy', allow_pickle=True)
+                self.performance_prediction_model = MilpGNNTrainable(config_dim=40, optimizer=None, initial_lr=None, batch_size=None, git_hash=None,
+                                                                     problem=None, n_gnn_layers=4,
+                                                                     gnn_hidden_dim=64, ensemble_size=3).load_from_checkpoint(model_path)
+            else:
+                raise ValueError(f"Problem {self.problem} unknown.")
+            self.performance_prediction_model.eval()
+
         if torch.cuda.is_available():
             self.performance_prediction_model.to(torch.device("cuda"))
 
@@ -55,33 +92,27 @@ class Policy():
         pl.seed_everything(seed)
 
     def __call__(self, action_set, observation):
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if self.problem == "anonymous":
+            best_config_id = self.best_config_id_prediction_model(observation)
+            return self._get_scip_parameter_configuration_by(best_config_id)
+        else:
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        graph = observation
+            instance_data = MilpBipartiteData(
+                var_feats=observation.variable_features,
+                cstr_feats=observation.constraint_features,
+                edge_indices=observation.edge_features.indices,
+                edge_values=observation.edge_features.values,
+            )
 
-        instance_data = MilpBipartiteData(
-            var_feats=graph.variable_features,
-            cstr_feats=graph.constraint_features,
-            edge_indices=graph.edge_features.indices,
-            edge_values=graph.edge_features.values,
-        )
+            instance_batch = Batch.from_data_list([instance_data], ).to(device)
 
-        config_batch = self._generate_config_data().to(device)
-        instance_batch = Batch.from_data_list([instance_data],).to(device)
+            best_config_tuple = EvaluatePredictedParametersCallback._find_best_configs(self.performance_prediction_model, instance_batch,
+                                                                                       self.config_index_to_tuple_mapping)["mean"][0]
+            assert (isinstance(best_config_tuple, tuple)) and len(best_config_tuple) == 4
+            best_config_id = self.config_tuple_to_id_mapping[best_config_tuple]
 
-        predictions, mean_mu, mean_var, epi_var = self.performance_prediction_model((instance_batch, config_batch),
-                                                                                    single_instance=True)
-
-        lowest_mean_index = int(torch.argmin(mean_mu))
-        return self._get_scip_parameter_configuration_by(lowest_mean_index)
-
-    def _generate_config_data(self) -> Tensor:
-        presolve_config_options = torch.tensor([0., 1., 2., 3.])
-        heuristic_config_options = torch.tensor([0., 1., 2., 3.])
-        separating_config_options = torch.tensor([0., 1., 2., 3.])
-        return torch.cartesian_prod(presolve_config_options,
-                                                   heuristic_config_options,
-                                                   separating_config_options)
+            return self._get_scip_parameter_configuration_by(best_config_id)
 
     def _get_scip_parameter_configuration_by(self, index: int) -> Dict[str, Any]:
         path = PARAMETER_CONFIGURATIONS_PATH + "config-" + str(index) + ".set"
