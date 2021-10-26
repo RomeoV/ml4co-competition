0ea70db21da384514908eb0203732381d87cd065
Claudio Ferrari <ferraric@ethz.ch>
2021-10-26 13:50:10 +0200

    adjust versions

--- bo_gnn/config/init.sh
+++ bo_gnn/config/init.sh
@@ -7,9 +7,9 @@ conda env create -n ml4co -f conda.yaml
 conda activate ml4co
 
 # additional installation commands go here
-pip install torch==1.9.1+cpu torchvision==0.10.1+cpu torchaudio==0.9.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
-pip install torch-scatter -f https://data.pyg.org/whl/torch-1.9.0+cpu.html
-pip install torch-sparse -f https://data.pyg.org/whl/torch-1.9.0+cpu.html
-pip install torch-geometric==1.7.2 -f https://data.pyg.org/whl/torch-1.9.0+cpu.html
+pip install torch==1.9.1+cpu torchvision==0.10.1+cpu torchaudio==0.10.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch-scatter -f https://data.pyg.org/whl/torch-1.9.1+cpu.html
+pip install torch-sparse -f https://data.pyg.org/whl/torch-1.9.1+cpu.html
+pip install torch-geometric==1.7.2 -f https://data.pyg.org/whl/torch-1.9.1+cpu.html
 
 conda deactivate
