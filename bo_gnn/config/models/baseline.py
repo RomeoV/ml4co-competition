074a8f21e8e7863d979a62632b62ebb9bbf14719
Romeo Valentin <romeo.valentin@math.ethz.ch>
2021-11-05 01:51:09 +0100

    Take a bunch of model options from architecture-v2

--- bo_gnn/config/models/baseline.py
+++ bo_gnn/config/models/baseline.py
@@ -2,6 +2,7 @@ from typing import Tuple
 import torch
 import torch.nn
 import torch_geometric as tg
+from torch_geometric.data import DataLoader
 import torch.nn.functional as F
 import numpy as np
 
@@ -11,7 +12,7 @@ from data_utils.dataset import MilpDataset
 
 
 class ConfigPerformanceRegressor(torch.nn.Module):
-    def __init__(self, config_dim, n_gnn_layers=4, gnn_hidden_dim=8):
+    def __init__(self, config_dim, n_gnn_layers=4, gnn_hidden_dim=64):
         super(ConfigPerformanceRegressor, self).__init__()
 
         self.milp_gnn = MilpGNN(
@@ -62,14 +63,34 @@ class MilpGNN(torch.nn.Module):
                 for i in range(self.n_gnn_layers - 1)
             ]
         )
-        self.pool = tg.nn.global_mean_pool
+
+        self.max_pool = tg.nn.global_max_pool
+
+        self.attention_layer_var = torch.nn.Sequential(
+            torch.nn.Linear(hidden_dim[0], hidden_dim[0] // 2),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim[0] // 2, 1),
+        )
+        self.attention_pool_var = tg.nn.GlobalAttention(self.attention_layer_var)
+
+        self.attention_layer_cstr = torch.nn.Sequential(
+            torch.nn.Linear(hidden_dim[1], hidden_dim[1] // 2),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim[1] // 2, 1),
+        )
+        self.attention_pool_cstr = tg.nn.GlobalAttention(self.attention_layer_cstr)
 
     def forward(self, x):
         for l in self.gnns:
             x = l(x)
-        x_var = self.pool(x.var_feats, x.var_batch_el)
-        x_cstr = self.pool(x.cstr_feats, x.cstr_batch_el)
-        x = torch.cat([x_var, x_cstr], axis=-1)
+
+        # Max pooling
+        x_var_emb = self.max_pool(x.var_feats, x.var_batch_el)
+        x_cstr_emb = self.max_pool(x.cstr_feats, x.cstr_batch_el)
+        # Attention pooling
+        x_var_emb2 = self.attention_pool_var(x.var_feats, x.var_batch_el)
+        x_cstr_emb2 = self.attention_pool_cstr(x.cstr_feats, x.cstr_batch_el)
+        x = torch.cat([x_var_emb, x_cstr_emb, x_var_emb2, x_cstr_emb2], axis=-1)
         return x
 
 
@@ -80,16 +101,24 @@ class GNNFwd(torch.nn.Module):
         out_dim: Tuple[int, int],
         residual=False,
         batch_norm=True,
+        additional_dense=False,
     ):
         super(GNNFwd, self).__init__()
         self.Conv = tg.nn.GraphConv
 
-        self.node_layer = self.Conv(
+        self.additional_dense = additional_dense
+        if additional_dense:
+            self.node_encoder = torch.nn.Sequential(torch.nn.Linear(in_dim[0], in_dim[0]), torch.nn.ReLU())
+            self.cstr_encoder = torch.nn.Sequential(torch.nn.Linear(in_dim[1], in_dim[1]), torch.nn.ReLU())
+
+        # We're using 'mean' aggregation, because I've seen one single comparison and 'mean' had better performance than 'add'
+        # Table 8 (Appendix) of Bengio et al: Benchmarking GNNs: https://arxiv.org/pdf/2003.00982.pdf
+        self.node_gnn = self.Conv(
             in_channels=in_dim[::-1],
             out_channels=out_dim[0],
             aggr="mean",
         )
-        self.cstr_layer = self.Conv(
+        self.cstr_gnn = self.Conv(
             in_channels=in_dim,
             out_channels=out_dim[1],
             aggr="mean",
@@ -101,9 +130,7 @@ class GNNFwd(torch.nn.Module):
 
         self.residual = residual
         if self.residual:
-            assert (
-                in_dim == out_dim
-            ), "For residual layers, in_dim and out_dim have to match"
+            assert in_dim == out_dim, "For residual layers, in_dim and out_dim have to match"
 
     def forward(self, data):
         x_node = data.var_feats
@@ -113,12 +140,14 @@ class GNNFwd(torch.nn.Module):
             x_node = self.node_batch_norm(x_node)
             x_cstr = self.cstr_batch_norm(x_cstr)
 
+        if self.additional_dense:
+            x_node = self.node_encoder(x_node)
+            x_cstr = self.cstr_encoder(x_cstr)
+
         edge_attr = data.edge_attr.unsqueeze(-1)
 
-        x_node_ = self.node_layer(
-            x=(x_cstr, x_node), edge_index=data.edge_index, edge_weight=edge_attr
-        )
-        x_cstr_ = self.cstr_layer(
+        x_node_ = self.node_gnn(x=(x_cstr, x_node), edge_index=data.edge_index, edge_weight=edge_attr)
+        x_cstr_ = self.cstr_gnn(
             x=(x_node, x_cstr),
             edge_index=data.edge_index.flip(-2),
             edge_weight=edge_attr,
